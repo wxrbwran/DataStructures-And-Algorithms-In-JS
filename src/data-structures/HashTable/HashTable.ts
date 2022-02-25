c6621a59b8159ae543fc5f25228e947ae0271b95
wuxiaoran <wuxiaoran@xinzhili.cn>
2022-02-25 18:22:06 +0800

    feat(hashtable): 为解决冲突的HashTable

--- src/data-structures/HashTable/HashTable.ts
+++ src/data-structures/HashTable/HashTable.ts
@@ -8,6 +8,65 @@ class HashTable {
     this.toStrFn = toStrFn;
     this.table = {};
   }
+
+  loseloseHashCode(key: Object): number {
+    if (typeof key === 'number') {
+      return key;
+    }
+    const tableKey = this.toStrFn(key);
+    let hash = 0;
+    for (let i = 0; i < tableKey.length; i++) {
+      hash += tableKey.charCodeAt(i);
+    }
+    return hash % 37;
+  }
+
+  hashCode(key: Object): number {
+    return this.loseloseHashCode(key);
+  }
+
+  put(key: Object, val: Object): boolean {
+    if (!!key && !!val) {
+      const position = this.hashCode(key);
+      this.table[position] = val;
+      return true;
+    }
+    return false;
+  }
+
+  get(key: Object): Object | undefined {
+    if (!!key) {
+      const position = this.hashCode(key);
+      return this.table[position];
+    }
+    return undefined;
+  }
+
+  remove(key: Object): boolean {
+    if (!!key) {
+      const position = this.hashCode(key);
+      delete this.table[position];
+      return true;
+    }
+    return false;
+  }
+
+  size(): number {
+    return Object.keys(this.table).length;
+  }
+
+  isEmpty(): boolean {
+    return this.size() === 0;
+  }
+
+  toString(): string {
+    let res = '';
+    const positions = Object.keys(this.table);
+    positions.forEach((position) => {
+      res += this.table[position];
+    });
+    return res;
+  }
 }
 
 export default HashTable;
