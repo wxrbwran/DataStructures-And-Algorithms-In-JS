57bd0a00a27dba30903df73c9f7d1dd72ca93fea
wuxiaoran <wxrbw@qq.com>
2022-03-21 16:44:32 +0800

    feat(string): 计数二进制子串

--- src/algorithms/string/countingBinarySubString.ts
+++ src/algorithms/string/countingBinarySubString.ts
@@ -5,21 +5,30 @@
 // 00110011 => 0011, 01, 1100, 10, 0011, 01
 export default function countingBinarySubString(s: string): number {
   const length = s.length;
-  let res = 0;
-  for (let i = 0; i < length; i++) {
-    for (let j = i + 2; j < length; j += 2) {
-      let newString = s.slice(i, j);
-      console.log('newString1', newString);
-      let newArr = newString.split('').reverse();
-      console.log('newArr', newArr.join(''));
-      const set = new Set(newArr);
-      if (newString.length > 1 && set.size === 2) {
-        console.log('newString2', newString);
-
-        res++;
-        break;
+  let res = [];
+  const rec = (subStr: string) => {
+    let prevStr = subStr[0];
+    let nextStr = subStr[1];
+    if (nextStr != prevStr) {
+      const pv = subStr.slice(0, 2);
+      // console.log(pv);
+      res.push(pv);
+    } else {
+      let i = 1;
+      while (nextStr === subStr[0] && i < subStr.length - 1) {
+        i++;
+        nextStr = subStr[i];
+      }
+      prevStr = subStr.slice(0, i);
+      nextStr = subStr.slice(i, i + prevStr.length);
+      if (nextStr[0] !== prevStr[0] && new Set(nextStr).size === 1) {
+        // console.log(`${prevStr}${nextStr}`);
+        res.push(`${prevStr}${nextStr}`);
       }
     }
+  };
+  for (let i = 0; i < length - 1; i++) {
+    rec(s.slice(i));
   }
-  return res;
+  return res.length;
 }
